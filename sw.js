765122fc2f724034d5c9ee833b1dbfb43fd5f3ca
Jameswang2024 <154941301+Jameswang2024@users.noreply.github.com>
2026-05-01 13:39:41 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="fruit-merge-hd-v1";
-const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE="fruit-merge-3d-pro-v1";
+const ASSETS=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./assets/wood_bg.jpg","./assets/cherry.png","./assets/strawberry.png","./assets/grape.png","./assets/orange.png","./assets/apple.png","./assets/lemon.png","./assets/kiwi.png","./assets/dragon.png","./assets/pineapple.png","./assets/watermelon.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
